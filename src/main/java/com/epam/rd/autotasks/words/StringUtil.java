3eff89a67dec872604f5a0602ad38a6136ebd948
Vitalii Kalinchyk <karlkory@gmail.com>
2022-07-15 22:22:00 +0300

    StringUtil

--- src/main/java/com/epam/rd/autotasks/words/StringUtil.java
+++ src/main/java/com/epam/rd/autotasks/words/StringUtil.java
@@ -4,19 +4,78 @@ import java.util.Arrays;
 
 public class StringUtil {
     public static int countEqualIgnoreCaseAndSpaces(String[] words, String sample) {
-        throw new UnsupportedOperationException();
+
+        if (sample==null || words==null || words.length==0)
+            return 0;
+
+        int count=0;
+        for (String s:words) {
+            if (s.trim().equalsIgnoreCase(sample.trim()))
+                count++;
+        }
+
+        return count;
     }
 
     public static String[] splitWords(String text) {
-        throw new UnsupportedOperationException();
+
+        if (text==null || text.length()==0 || text.matches("\\W*"))
+            return null;
+
+        return text.replaceAll("\\W"," ").trim().split("\\s+");
     }
 
     public static String convertPath(String path, boolean toWin) {
-        throw new UnsupportedOperationException();
+
+        if (returnNull(path)) {
+            System.out.println("check");
+            return null;
+        }
+
+        if (path.matches("\\w*[.]\\w*")
+                || (path.contains("/") && (!toWin))
+                || path.contains("\\") && (toWin))
+            return path;
+
+        if (toWin) {
+            if (path.equals("/"))
+                return ("C:\\");
+            return path.replace("/root","C:\\root")
+                    .replace("~", "C:\\User")
+                    .replace("/", "\\");
+        }
+
+        if (path.equals("C:\\"))
+            return ("/");
+
+        return path.replace("C:\\User","~")
+                .replace("User","~")
+                .replace("C:\\root","/root")
+                .replace("C:","/root")
+                .replace("\\","/");
+    }
+
+    private static boolean returnNull(String path){
+
+        return path == null || path.length()==0
+                || (path.contains("/") || path.contains("~")) && (path.contains("\\") || path.contains("C:"))
+                || path.length()>1 && path.substring(1).contains("~")
+                || path.length()>2 && path.substring(2).contains("C:");
     }
 
     public static String joinWords(String[] words) {
-        throw new UnsupportedOperationException();
+
+        if (words==null || words.length==0 || String.join(" ", words).trim().length()==0)
+            return null;
+        StringBuilder result = new StringBuilder("[");
+
+        for (String s:words) {
+            if (s==null || s.length()==0)
+                continue;
+            result.append(s).append(", ");
+        }
+
+        return result.substring(0,result.length()-2)+"]";
     }
 
     public static void main(String[] args) {
